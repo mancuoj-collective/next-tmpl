267ef428d0db29c83e78b2289a68d6e5860132e6
mancuoj <mancuoj@gmail.com>
2024-11-18 00:59:45 +0800

    chore: update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import '@/styles/globals.css'
 
 import type { Metadata } from 'next'
 
@@ -10,9 +10,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body>{children}</body>
