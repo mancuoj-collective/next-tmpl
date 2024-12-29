43291d2100d6567845873eebd4c552285115ac3f
mancuoj <mancuoj@gmail.com>
2024-12-29 15:27:32 +0800

    feat: site config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,13 +6,42 @@ import { NextIntlClientProvider } from 'next-intl'
 import { getLocale, getMessages } from 'next-intl/server'
 
 import { Header } from '@/components/header'
+import { site } from '@/config/site'
 import { cn } from '@/lib/utils'
 
 import { AppProvider } from './provider'
 
 export const metadata: Metadata = {
-  title: 'Next',
-  icons: [{ rel: 'icon', url: '/favicon.ico' }],
+  metadataBase: new URL(site.url),
+  title: {
+    default: site.name,
+    template: `%s | ${site.name}`,
+  },
+  description: site.description,
+  keywords: site.keywords,
+  robots: { index: true, follow: true },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-32x32.png',
+    apple: '/apple-touch-icon.png',
+  },
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: site.url,
+    title: site.name,
+    description: site.description,
+    siteName: site.name,
+    images: '/og.png',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: site.name,
+    description: site.description,
+    images: '/twitter.png',
+    site: site.url,
+    creator: site.author,
+  },
 }
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
--- src/middleware.ts
+++ src/middleware.ts
@@ -13,5 +13,5 @@ export default auth((req) => {
 })
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|og.png|twitter.png|favicon-32x32.png|apple-touch-icon.png).*)'],
 }
