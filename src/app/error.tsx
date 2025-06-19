0e5f18dc82d7f085cf7e6d0d8ea65bc3b3908eb8
mancuoj <mancuoj@gmail.com>
2025-06-19 20:15:40 +0800

    chore: update

---
+++ src/app/error.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import Link from 'next/link'
+import { useEffect } from 'react'
+
+import { Button } from '@/components/ui/button'
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    console.error(error)
+  }, [error])
+
+  return (
+    <div className="flex h-svh flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">500 Server Error</h1>
+      <div className="mt-8 flex items-center gap-2.5">
+        <Button variant="outline" onClick={reset}>
+          Refersh
+        </Button>
+        <Button variant="outline" asChild>
+          <Link href="/">Back to Home</Link>
+        </Button>
+      </div>
+    </div>
+  )
+}
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,9 +4,9 @@ import { Button } from '@/components/ui/button'
 
 export default function NotFound() {
   return (
-    <div className="flex h-svh flex-col items-center justify-center gap-8">
+    <div className="flex h-svh flex-col items-center justify-center">
       <h1 className="text-4xl font-bold">404 Not Found</h1>
-      <Button variant="outline" asChild>
+      <Button variant="outline" asChild className="mt-8">
         <Link href="/">Back to Home</Link>
       </Button>
     </div>
