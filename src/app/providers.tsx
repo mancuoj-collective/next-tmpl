5dfa6f0633a63d2cb7f32c81f65e77540726ce08
mancuoj <mancuoj@gmail.com>
2025-06-25 22:28:41 +0800

    feat: add nuqs

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,14 @@
+import { NuqsAdapter } from 'nuqs/adapters/next/app'
+
 import { Toaster } from '@/components/shadcn/sonner'
 import { ThemeProvider } from '@/components/theme/provider'
 
 export function RootProvider({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider>
-      {children}
+      <NuqsAdapter>
+        {children}
+      </NuqsAdapter>
       <Toaster position="top-right" richColors />
     </ThemeProvider>
   )
