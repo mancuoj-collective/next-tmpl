93841bf65746c1625a443feb1fdfc95634f21a5a
mancuoj <mancuoj@gmail.com>
2025-06-27 20:31:11 +0800

    feat: tailwind indicator

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,7 @@
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
 
 import { Toaster } from '@/components/shadcn/sonner'
+import { TailwindIndicator } from '@/components/tailwind-indicator'
 import { ThemeProvider } from '@/components/theme/provider'
 
 export function RootProvider({ children }: { children: React.ReactNode }) {
@@ -8,8 +9,9 @@ export function RootProvider({ children }: { children: React.ReactNode }) {
     <ThemeProvider>
       <NuqsAdapter>
         {children}
+        <TailwindIndicator />
+        <Toaster position="top-right" richColors />
       </NuqsAdapter>
-      <Toaster position="top-right" richColors />
     </ThemeProvider>
   )
 }
