82aae0caa9e4e40a4309a44609c359b71dc32d2e
mancuoj <mancuoj@gmail.com>
2025-06-29 23:42:00 +0800

    feat: server loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,16 +1,6 @@
-'use client'
-
-import { useSpinDelay } from 'spin-delay'
-
 import { WaveSpinner } from '@/components/wave-spinner'
 
 export default function Loading() {
-  const showSpinner = useSpinDelay(true, { delay: 500, minDuration: 500 })
-
-  if (!showSpinner) {
-    return null
-  }
-
   return (
     <div className="flex h-svh flex-col items-center justify-center">
       <WaveSpinner className="size-16" />
