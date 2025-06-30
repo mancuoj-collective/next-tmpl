d43ccd62172427fd0bad5371400073159da80304
mancuoj <mancuoj@gmail.com>
2025-06-30 18:54:08 +0800

    refactor: map

--- src/components/auth/footer.tsx
+++ src/components/auth/footer.tsx
@@ -8,30 +8,30 @@ interface AuthFooterProps {
   mode: AuthMode
 }
 
+const footerMap = {
+  'sign-in': {
+    text: 'Don\'t have an account?',
+    link: '/sign-up',
+    linkText: 'Sign Up Now',
+  },
+  'sign-up': {
+    text: 'Have an account?',
+    link: '/sign-in',
+    linkText: 'Sign In Now',
+  },
+  'forgot-password': {
+    text: 'Already have an account?',
+    link: '/sign-in',
+    linkText: 'Sign In',
+  },
+}
+
 export function AuthFooter({ mode }: AuthFooterProps) {
-  if (mode === 'sign-in') {
-    return (
-      <div className="my-8 text-center text-sm">
-        <span className="text-foreground/70">Don't have an account? </span>
-        <Link href="/sign-up" className="underline hover:text-muted-foreground">Sign Up Now</Link>
-      </div>
-    )
-  }
-  if (mode === 'sign-up') {
-    return (
-      <div className="my-8 text-center text-sm">
-        <span className="text-foreground/70">Already have an account? </span>
-        <Link href="/sign-in" className="underline hover:text-muted-foreground">Sign In Now</Link>
-      </div>
-    )
-  }
-  if (mode === 'forgot-password') {
-    return (
-      <div className="my-8 text-center text-sm">
-        <span className="text-foreground/70">Already have an account? </span>
-        <Link href="/sign-in" className="underline hover:text-muted-foreground">Sign In</Link>
-      </div>
-    )
-  }
-  return null
+  const { text, link, linkText } = footerMap[mode]
+  return (
+    <div className="my-8 text-center text-sm">
+      <span className="text-foreground/70">{text}{' '}</span>
+      <Link href={link} className="underline hover:text-muted-foreground">{linkText}</Link>
+    </div>
+  )
 }
