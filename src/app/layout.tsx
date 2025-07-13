4795d836a2ae344018e8fc95ad6de55419f94b8a
mancuoj <mancuoj@gmail.com>
2025-07-13 18:14:25 +0800

    feat: remove react scan

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,9 +45,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <script src="https://unpkg.com/react-scan/dist/auto.global.js" />
-      </head>
       <body
         className={cn(
           fontSans.variable,
