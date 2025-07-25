998819831046235c8fcc90185a57156a281d0ee0
mancuoj <mancuoj@gmail.com>
2025-07-26 03:09:23 +0800

    feat: all dm fonts

--- src/config/font.ts
+++ src/config/font.ts
@@ -1,5 +1,5 @@
-import { DM_Sans, JetBrains_Mono, Lora } from 'next/font/google'
+import { DM_Mono, DM_Sans, DM_Serif_Display } from 'next/font/google'
 
 export const fontSans = DM_Sans({ variable: '--font-sans', subsets: ['latin'] })
-export const fontMono = JetBrains_Mono({ variable: '--font-mono', subsets: ['latin'] })
-export const fontSerif = Lora({ variable: '--font-serif', subsets: ['latin'] })
+export const fontMono = DM_Mono({ variable: '--font-mono', subsets: ['latin'], weight: ['300', '400', '500'] })
+export const fontSerif = DM_Serif_Display({ variable: '--font-serif', subsets: ['latin'], weight: ['400'] })
